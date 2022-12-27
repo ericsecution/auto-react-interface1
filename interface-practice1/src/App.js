39f6a8eece615beef8b8afd8042b088884122caf
Eric Mangin <ericpaulmangin@gmail.com>
2022-12-27 15:47:36 -0700

    setup an initial (working) sorting functionality for my search box in App.js file

--- interface-practice1/src/App.js
+++ interface-practice1/src/App.js
@@ -6,6 +6,17 @@ import AppointmentInfo from './components/AppointmentInfo'
 
 function App() {
   let [appointmentList, setAppointmentList] = useState([])
+  let [query, setQuery] = useState("")
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -26,11 +37,12 @@ function App() {
 				Appointments
 			</h1>
       <AddAppointment />
-			<Search />
+			<Search query={ query } 
+        onQueryChange={ myQuery => setQuery(myQuery) } />
 
 
       <ul className='divide-y divide-gray-200'>
-        { appointmentList 
+        { filteredAppointments 
           .map(appointment => (
               <AppointmentInfo key={appointment.id}
               appointment={appointment}
