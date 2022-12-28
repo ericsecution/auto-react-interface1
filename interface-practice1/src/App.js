9fc218ee71b4416b331f15b46c5b4f92a6fefc85
Eric Mangin <ericpaulmangin@gmail.com>
2022-12-27 17:16:00 -0700

    Updated my Search.js and App.js so that I could sort by petName, ownerName, or aptDate--either ascending or descending order

--- interface-practice1/src/App.js
+++ interface-practice1/src/App.js
@@ -41,12 +41,15 @@ function App() {
 	return (
 		<div className='App container mx-auto mt-3 font-thin'>
 			<h1 className='text-5xl mb-3 underline'>
-				<BiCalendar className='inline-block text-red-400 align-top' /> Your
-				Appointments
-			</h1>
+				<BiCalendar className='inline-block text-red-400 align-top' /> Your Appointments</h1>
       <AddAppointment />
 			<Search query={ query } 
-        onQueryChange={ myQuery => setQuery(myQuery) } />
+        onQueryChange={ myQuery => setQuery(myQuery) }
+        orderBy={ orderBy }
+        onOrderByChange={ mySort => setOrderBy(mySort) }
+        sortBy={ sortBy }
+        onSortByChange={ mySort => setSortBy(mySort) }
+      />
 
 
       <ul className='divide-y divide-gray-200'>
